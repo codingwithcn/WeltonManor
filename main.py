f86cb94649116af12d69032fba7d919862663ded
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2022-04-13 21:32:29 +0000

    done making the template site for welton manor

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ app.secret_key = ":sddsadasdsa"
 def home():
   return render_template('home.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+  return render_template('about.html')
+
+@app.route('/ratings', methods=['GET'])
+def ratings():
+  return render_template('ratings.html')
+
+@app.route('/images', methods=['GET'])
+def images():
+  return render_template('images.html')
+
 if __name__ == '__main__':
   app.run(
     host = '0.0.0.0',
